25ed78373f6f8afc91e2a113a1e6f11641ed6d72
lixinghe1999 <helixing99@gmail.com>
2025-05-17 16:05:05 +0800

    clean the dataset to be a full localization repo

--- models/beamforming/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Convtasnet.net import ConvTas_Net
-from .deepbeam.net import BeamformerModel
-from .tfgridnet_realtime.net import Net
-from .tfgridnet_realtime_embed.net import Net as Net_embed
--- models/separation/__init__.py
+++
@@ -1,2 +0,0 @@
-from .semantichearing.dcc_tf import Net as SemanticHearingNet 
-from .semantichearing.dcc_tf_binaural import Net as SemanticHearingNetBinaural
